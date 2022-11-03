010f6a758bf46a66107fefdf4ef549dd8572e59d
violeta atkinson <violeta.atkinson@gmail.com>
2022-11-03 10:41:41 +0100

    modified lenght of content adopted

--- models/Adopted.model.js
+++ models/Adopted.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const adoptedSchema = new Schema ({
+   
     image : {
         type: String,
         default:"https://res.cloudinary.com/plasoironhack/image/upload/v1644663323/ironhack/multer-example/icono-de-li%CC%81nea-perfil-usuario-si%CC%81mbolo-empleado-avatar-web-y-disen%CC%83o-ilustracio%CC%81n-signo-aislado-en-fondo-blanco-192379539_jvh06m.jpg"       
@@ -9,8 +10,8 @@ const adoptedSchema = new Schema ({
     content: {
         type: String,
         required: true,
-        maxLength: [50, 'To long'],
     } 
+
 },{timestamps: true })
 
 const Adopted = mongoose.model('Adopted', adoptedSchema);
