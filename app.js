d76a960e99fb60d3f9ef9e664b13ee775eceecdb
violeta atkinson <violeta.atkinson@gmail.com>
2022-11-16 18:00:09 +0100

    test

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json()); // Sin esto no sabe usar req.body
 
 // CORS middleware
 app.use((req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Headers", "content-type, Authorization");
   res.set("Access-Control-Allow-Methods", "*");
   res.set("Access-Control-Allow-Credentials", "true");
