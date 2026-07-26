3d4a3d3d60f8e90f091141d24f7cfbfd77974fd5
violeta atkinson <violeta.atkinson@gmail.com>
2026-07-26 12:09:21 -0700

    fix(like): point Like/Dislike ref at Pet model instead of removed Adoption model

--- models/Dislike.model.js
+++ models/Dislike.model.js
@@ -2,17 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const dislikeSchema = new Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    adoption:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Adoption',
-        required: true
-    }
-}, {timestamps: true });
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    adoption: { type: mongoose.Schema.Types.ObjectId, ref: 'Pet', required: true }
+}, { timestamps: true });
 
-const Dislike = mongoose.model('dislike', dislikeSchema);
+const Dislike = mongoose.model('Dislike', dislikeSchema); // antes 'dislike' en minúscula, sin motivo
 module.exports = Dislike;
--- models/Like.model.js
+++ models/Like.model.js
@@ -1,18 +1,11 @@
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const likeSchema = new Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    adoption:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Adoption',
-        required: true
-    }
-}, {timestamps: true });
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    adoption: { type: mongoose.Schema.Types.ObjectId, ref: 'Pet', required: true }
+}, { timestamps: true });
 
 const Like = mongoose.model('Like', likeSchema);
 module.exports = Like;
