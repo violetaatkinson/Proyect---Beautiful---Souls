6ed39622ba123025b441e455749a8ae2f5a8fb02
violeta atkinson <violeta.atkinson@gmail.com>
2026-07-26 12:10:05 -0700

    fix(misc): require Pet model instead of removed Adoption.model

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -1,53 +1,49 @@
-const Like = require('../models/Like.model')
- const Dislike = require('../models/Dislike.model')
-const { sendNotification } = require('../services/notificationService')
-const Adoption = require('../models/Adoption.model')
+const Like = require('../models/Like.model');
+const Dislike = require('../models/Dislike.model');
+const { sendNotification } = require('../services/notificationService');
+const Pet = require('../models/Pet.model');
 
-
-
- module.exports.likesList = (req, res, next) => {
-     Like.find({ user: req.currentUser })
-        .populate({
-            path: 'adoption',
-            populate: { path: 'owner', select: 'userName email phoneNumber image' }
-        })
-     .then(likes => {
-        res.json(likes.map(like => like.adoption))
+module.exports.likesList = (req, res, next) => {
+  Like.find({ user: req.currentUser })
+    .populate({
+      path: 'adoption',
+      populate: { path: 'owner', select: 'userName email phoneNumber image' },
     })
-    .catch(next)
-}
+    .then((likes) => res.json(likes.map((like) => like.adoption)))
+    .catch(next);
+};
 
 module.exports.likes = (req, res, next) => {
-    const adoptionId = req.params.id
+  const petId = req.params.id;
 
-    Like.findOneAndDelete({ adoption: adoptionId, user: req.currentUser })
-        .then(like => {
-            if (like) {
-                res.status(200).json({ success : 'Like remove from DDBB'})
-            }  else {
-                return Like.create({ adoption: adoptionId, user: req.currentUser })
-                    .then((like) => {
-                        Adoption.findById(like.adoption)
-                            .then(adoption => {
-                                sendNotification({ receiver: adoption.owner, user: req.currentUser, type: 'Like', title: 'liked your pet' })
-                                Dislike.findOneAndDelete({ adoption: adoptionId, user: req.currentUser })
-                                    .then(() => res.status(201).json({ success : 'Like added to DDBB' }))
-                            })
-                       
-                })
-            }
+  Like.findOneAndDelete({ adoption: petId, user: req.currentUser })
+    .then((like) => {
+      if (like) {
+        return res.status(200).json({ success: 'Like removed from DB' });
+      }
+
+      return Like.create({ adoption: petId, user: req.currentUser }).then((newLike) =>
+        Pet.findById(newLike.adoption).then((pet) => {
+          sendNotification({
+            receiver: pet.owner,
+            user: req.currentUser,
+            type: 'Like',
+            title: 'liked your pet',
+          });
+
+          return Dislike.findOneAndDelete({ adoption: petId, user: req.currentUser }).then(() =>
+            res.status(201).json({ success: 'Like added to DB' })
+          );
         })
-    .catch((err) => {
-        console.log(err)
-        next(err)
+      );
     })
-
-}
+    .catch(next);
+};
 
 module.exports.dislikes = (req, res, next) => {
-   const adoptionId = req.params.id
+  const petId = req.params.id;
 
-   Dislike.create({ adoption: adoptionId, user: req.currentUser })
-    .then(() => res.status(201).json({ success : 'Dislike successfully created'}))
-    .catch((e) => next(e))
-}
+  Dislike.create({ adoption: petId, user: req.currentUser })
+    .then(() => res.status(201).json({ success: 'Dislike successfully created' }))
+    .catch(next);
+};
