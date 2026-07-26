fa6d5c4357f5958466037e8d45d50757b5da0dc1
violeta atkinson <violeta.atkinson@gmail.com>
2026-07-26 12:07:22 -0700

    fix(user): restore users.controller.js with ownership checks and point it at Pet model

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,105 +1,85 @@
 const createError = require('http-errors');
 const User = require('../models/User.model');
-const Like = require('../models/Like.model.js');
-const Adoption = require('../models/Adoption.model');
+const Like = require('../models/Like.model');
+const Pet = require('../models/Pet.model');
 
+
+const REGISTER_WRITABLE_FIELDS = [
+  'userName', 'email', 'password', 'firstName', 'lastName',
+  'age', 'gender', 'phoneNumber', 'accountType', 'shelterName',
+  'city', 'province',
+];
+
+const PROFILE_WRITABLE_FIELDS = [
+  'userName', 'firstName', 'lastName', 'age', 'gender',
+  'phoneNumber', 'accountType', 'shelterName', 'city', 'province',
+];
+
+const pickFields = (body, allowedFields) =>
+  allowedFields.reduce((acc, field) => {
+    if (body[field] !== undefined) acc[field] = body[field];
+    return acc;
+  }, {});
 
 module.exports.list = (req, res, next) => {
-    User.find({ _id: { $ne: req.currentUser } }) // buscamos los usuarios menos yo
-      .then(users => { // promesa los encontramos
-        res.json(users)// los devolvemos
-      })
-      .catch(next)
-  }
-
-  /*
- .populate({
-      path: "like",
-      populate:{
-        path: "user"
-      }
-    })// buscamos los usuarios
-  */
+  User.find({ _id: { $ne: req.currentUser } })
+    .then((users) => res.json(users))
+    .catch(next);
+};
 
 module.exports.listWithLikes = (req, res, next) => {
-  Adoption.find({ owner: req.currentUser })
-  .populate({path: "like", populate: { path:"user"}})
-    .then(adoptions => {
-      const result = adoptions.reduce((acc, adoption) => {
-        const users = adoption.like.map(like => like.user)
-        acc = [...acc, ...users]
-        return acc
-      },[])
-      const set = new Set(result) 
-      const arrSet = Array.from(set)// promesa los encontramos
-      res.json(arrSet)// los devolvemos
+  Pet.find({ owner: req.currentUser })
+    .populate({ path: 'like', populate: { path: 'user' } })
+    .then((pets) => {
+      const result = pets.reduce((acc, pet) => {
+        const users = pet.like.map((like) => like.user);
+        return [...acc, ...users];
+      }, []);
+
+      res.json(Array.from(new Set(result)));
     })
-    .catch(next)
-}
+    .catch(next);
+};
 
+module.exports.create = (req, res, next) => {
+  const user = pickFields(req.body, REGISTER_WRITABLE_FIELDS);
 
+  if (req.file) user.image = req.file.path;
 
-  module.exports.create = (req, res, next) => {
-    const user = {
-      ...req.body,
-    };
+  User.create(user)
+    .then((createdUser) => res.status(201).json(createdUser))
+    .catch(next);
+};
 
-    if (req.file) {
-      user.image = req.file.path;
-    }
-    
-    User.create(req.body) // del front del formulario
-      .then(user => // se crea el usuario
-         res.status(201).json(user)) // devolvemos el usuario creado
-      .catch(next)
-  }
+module.exports.getCurrentUser = (req, res, next) => {
+  User.findById(req.currentUser)
+    .then((user) => {
+      if (!user) return next(createError(404, 'User not found'));
+      res.json(user);
+    })
+    .catch(next);
+};
 
-
-
-  module.exports.getCurrentUser = (req, res, next) => {
-    User.findById(req.currentUser) // encontramos al user x el id
-      .then(user => { // una vez encontrado
-        if (!user) { // si no hay user
-          next(createError(404, 'User not found')); // devolvemos un error
-        } else { // si existe
-          res.json(user); // devolvemos al usuario encontrado
-        }
-      })
-      .catch(next)
-  }
-
- 
 module.exports.edit = (req, res, next) => {
-  const userToUpdate = req.body
-  if (req.file) {
-    userToUpdate.image = req.file.path;
-  }
-  User.findByIdAndUpdate(req.params.id, userToUpdate, { new: true })
-    .then(user => {
-      console.log(user, userToUpdate)
-      res.status(200).json(user)
-    })
-    .catch(next)
-}
+  // Un usuario solo puede editar su propia cuenta, sin importar
+  // qué :id venga en la URL.
+  const updates = pickFields(req.body, PROFILE_WRITABLE_FIELDS);
+  if (req.file) updates.image = req.file.path;
 
-module.exports.delete = (req, res, next) => {
-  User.findByIdAndDelete(req.params.id)
+  User.findByIdAndUpdate(req.currentUser, updates, { new: true, runValidators: true })
     .then((user) => res.status(200).json(user))
     .catch(next);
-}
+};
+
+module.exports.delete = (req, res, next) => {
+  User.findByIdAndDelete(req.currentUser)
+    .then((user) => res.status(200).json(user))
+    .catch(next);
+};
 
 module.exports.profile = (req, res, next) => {
-  console.log(req.currentUser)
   Like.find({ user: req.currentUser })
-    .populate("adoption")
-    .then(likedAdoption => {
-      res.status(200).json(likedAdoption)})
-      .catch(next)
-}
-
-
-
-
-
-
-
+    .populate('adoption')
+    .then((likedPets) => res.status(200).json(likedPets))
+    .catch(next);
+};
