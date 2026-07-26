92b354edc1e7b45bcf80f7aa77716a936fd7197c
violeta atkinson <violeta.atkinson@gmail.com>
2026-07-26 11:30:50 -0700

    chore(shared): extract default avatar url to shared constant

---
+++ constants/defaults.js
@@ -0,0 +1,2 @@
+module.exports.DEFAULT_AVATAR_URL =
+  'https://res.cloudinary.com/plasoironhack/image/upload/v1644663323/ironhack/multer-example/icono-de-li%CC%81nea-perfil-usuario-si%CC%81mbolo-empleado-avatar-web-y-disen%CC%83o-ilustracio%CC%81n-signo-aislado-en-fondo-blanco-192379539_jvh06m.jpg';
